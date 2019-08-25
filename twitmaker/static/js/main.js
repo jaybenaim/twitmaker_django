3fa9f854d2848a98f288375600ac07955bf3d6fd
jaybenaim <benaimjacob@gmail.com>
2019-08-25 19:47:51 -0400

    temporary delete button created

--- twitmaker/static/js/main.js
+++ twitmaker/static/js/main.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   let form = document.querySelector('form');
-  let body = document.querySelector('body')
   let tweets = document.querySelector('.tweets')
   let li = document.createElement('li');
+  let deleteButton = document.querySelectorAll('#delete')
 
+ 
   form.addEventListener('submit', function(event) {
     event.preventDefault();
     let formData = new FormData(this);
@@ -13,12 +14,30 @@ document.addEventListener('DOMContentLoaded', function() {
       { headers: { "X-Requested-With": "XMLHttpRequest" } }
     )
     .then(response => {
+      let date = new Date()
+      data = { 
+        message: this.message.value, 
+        date: date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear(),
+        }
       tweets.innerHTML = ''
       tweets.appendChild(li)
-      li.innerHTML = JSON.stringify(this.message.value)
-    
+      li.innerHTML = JSON.stringify(data)
+
+      console.log(date)
+      
     }).catch(function(error) {
       console.log(error);
-    });
+    })
+   
   });
+  
+  deleteButton.forEach(button => { 
+    button.addEventListener('click', function() { 
+      let b = button.parentNode.parentNode
+        b.remove() 
+    }) 
+
+
+  })
+
 });
